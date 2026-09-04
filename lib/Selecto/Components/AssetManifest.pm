343d45c8f00b1e9c193b343a4f2474a538eef9c8
Chris Rohlfs <seeken@gmail.com>
2026-09-04 15:51:37 -0600

    Refresh shared browser assets

--- lib/Selecto/Components/AssetManifest.pm
+++ lib/Selecto/Components/AssetManifest.pm
@@ -5,7 +5,7 @@ use warnings;
 use Exporter qw(import);
 
 our @EXPORT_OK = qw(asset_revision);
-my $ASSET_REVISION = '0.1.0-a7557290328a';
+my $ASSET_REVISION = '0.1.0-597b83430ebb';
 
 sub asset_revision { return $ASSET_REVISION; }
 
